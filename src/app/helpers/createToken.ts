22e2e788b9305683fe3ccc69a29e667e4bb91431
Ahmad Saleem <asaleem029@gmail.com>
2025-01-29 00:35:42 +0500

    Verify JWT token

--- src/app/helpers/createToken.ts
+++ src/app/helpers/createToken.ts
@@ -1,15 +1,13 @@
 // import { SignOptions, sign } from 'jsonwebtoken';
 import jwt from 'jsonwebtoken';
-import { ApiError, ApiSuccess, JwtSettings } from '../utils/index.js';
+import { ApiError, ApiSuccess, JwtSettings, TOKEN_SECRET } from '../utils/index.js';
 import { StatusCodes } from 'http-status-codes';
 
 const CREATE_TOKEN = async (user: any, secret: any): Promise<IAPISuccessResponse> => {
   try {
     const TOKEN = jwt.sign(user, secret as string, {
       algorithm: JwtSettings.ALGORITHM as any,
-      expiresIn: JwtSettings.TOKEN_EXPIRY as string,
-      issuer: JwtSettings.ISSUER as string,
-      audience: JwtSettings.AUDIENCE as string,
+      expiresIn: JwtSettings.TOKEN_EXPIRY as string
     });
 
     return ApiSuccess.format({
